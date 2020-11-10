9ddce197a78f66c7148a672d4ad36872ef48aa90
Artem Laptiev <artuon@gmail.com>
2020-11-10 22:42:44 +0200

    TailWind Demo: Fix URL

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -3,10 +3,38 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: {
+    mode: 'all',
+    content: [
+      // Paths to your templates here...
+    ],
+  },
   theme: {
-    extend: {},
+    screens: {
+      'sm': {'min': '1px', 'max': '767px'},
+      'md': {'min': '768px'},
+      'lg': {'min': '1024px'},
+      'xl': {'min': '1280px'},
+    },
+    extend: {
+      fontFamily: {
+        'nunito': ['nunito', 'sans-serif']
+      },
+      colors: {
+        'cst-dark': {
+          lighter: '#3d5a80',
+          default: '#293241',
+        },
+        'cst-light': {
+          darker: '#98c1d9',
+          default: '#e0fbfc',
+        },
+        'cst-orange': '#ee6c4d',
+      },
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
