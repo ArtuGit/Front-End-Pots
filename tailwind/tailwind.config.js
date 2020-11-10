965841da151496020cd4be420434f55ee9486ddd
Artem Laptiev <artuon@gmail.com>
2020-11-10 23:12:56 +0200

    TailWind Boilerplate: Create an empty boilerplate from Sandbox

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -16,25 +16,7 @@ module.exports = {
       'lg': {'min': '1024px'},
       'xl': {'min': '1280px'},
     },
-    extend: {
-      fontFamily: {
-        'nunito': ['nunito', 'sans-serif']
-      },
-      colors: {
-        'cst-dark': {
-          lighter: '#3d5a80',
-          default: '#293241',
-        },
-        'cst-light': {
-          darker: '#98c1d9',
-          default: '#e0fbfc',
-        },
-        'cst-orange': '#ee6c4d',
-      },
-    },
   },
   variants: {},
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [],
 }
